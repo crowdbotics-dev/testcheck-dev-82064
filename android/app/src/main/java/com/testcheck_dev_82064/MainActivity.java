a16e2221428f1e7b4b8f66c0b5e3c047b0d84d32
Crowdbotics <engineering@crowdbotics.com>
2023-05-08 10:16:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testcheck_dev_82064/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testcheck_dev_82064;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "testcheck_dev_82064";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testcheck-dev-82064",
+  url: "https://testcheck-dev-82064.botics.co",
+  api: "https://testcheck-dev-82064.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
